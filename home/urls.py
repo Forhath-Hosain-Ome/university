c86062a8c3fdfb5c71332b1a155aaf0a777ad705
Forhath-Hosain-Ome <omehasan3@gmail.com>
2025-05-21 18:39:31 +0600

    Nav And Footer Done

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,7 @@ app_name = 'home'  # Namespace for URL reversing
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/index.html')
+    return render(request, 'home/index.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+def privacy_policy(request):
+    return render(request, 'home/privacy_policy.html')
