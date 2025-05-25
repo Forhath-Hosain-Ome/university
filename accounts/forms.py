7d1e42511975d9245ca4b389f15df2fc94a12fff
Forhath-Hosain-Ome <omehasan3@gmail.com>
2025-05-25 23:38:05 +0600

    login completed

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import *
+
+class registerForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(attrs={'placeholder': 'Enter your password','class' : 'form-control'}),
+            'username': forms.TextInput(attrs={'placeholder': 'Enter your username', 'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter your email', 'class': 'form-control'}),
+        }
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Account.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists.")
+        return email
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if Account.objects.filter(username=username).exists():
+            raise forms.ValidationError("Username already exists.")
+        return username
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import registerForm
+
 
 # Create your views here.
 def login(request):
     return render(request, 'accounts/login.html')
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    form = registerForm()
+    if request.method == 'POST':
+        form = registerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home:home')
+    else:
+        form = registerForm()
+    return render(request, 'accounts/register.html', {'form' : form})
 
 def profile(request):
     return render(request, 'accounts/profile.html')
