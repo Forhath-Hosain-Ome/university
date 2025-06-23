7aba76e02ee4247813b6a97f55e062a11b226178
Forhath Hosain Ome <62540867+Forhath-Hosain-Ome@users.noreply.github.com>
2025-06-23 06:16:36 +0000

    view work

---
+++ accounts/views/auths.py
@@ -0,0 +1,5 @@
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from rest_framework import authentication, permissions
+# from accounts.models import User
+
